4c483b5bfb5984e34ed19087ecbc20f3e93cae4b
DOC-weed <aleesfeo1234@gmail.com>
2021-11-30 17:55:14 -0600

    firstCommit

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { TouchableOpacity, Text } from 'react-native';
+
+import Tienda from './pages/Tienda/Tienda';
+import Login from './pages/Login/Login';
+import Registro from './pages/Registro/Registro';
+import Perfil from './pages/Perfil/Perfil';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        if (route.name === "Tienda") {
+          iconName = focused
+            ? "ios-book"
+            : "ios-book-outline";
+        } else if (route.name === "Perfil") {
+          iconName = focused
+            ? "ios-person"
+            : "ios-person-outline";
+        }
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: "#5F366E",
+      tabBarInactiveTintColor: "gray",
+    })}>
+      <Tab.Screen name="Tienda" component={Tienda} />
+      <Tab.Screen name="Perfil" component={Perfil}  />
+    </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ pages/Tienda/Tienda.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function Tienda() {
+  return(
+    <Text>Tienda</Text>
+  );
+}
