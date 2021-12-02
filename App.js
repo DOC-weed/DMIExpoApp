e1fab8b9e194116d7a9a02bde2a222c36ae2b424
JonhyGuy <joulura1609@gmail.com>
2021-12-02 17:57:41 -0600

    Login y Registro

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { auth } from './firebase';
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { TouchableOpacity, Text } from 'react-native';
 
@@ -12,12 +15,46 @@ import Login from './pages/Login/Login';
 import Registro from './pages/Registro/Registro';
 import Perfil from './pages/Perfil/Perfil';
 
-
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
+
   return (
     <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" options={{ headerShown: false }}>
+          {(props) => <Login {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Singup" options={{ headerShown: false }}>
+          {(props) => <Registro {...props} />}
+        </Stack.Screen>
+
+        <Stack.Screen
+          name= 'Tabs'
+          options={{ headerShown: false }}
+          component={Tabs}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Tabs() {
+  const navigation = useNavigation();
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+  return(
     <Tab.Navigator screenOptions={({ route }) => ({
       tabBarIcon: ({ focused, color, size }) => {
         let iconName;
@@ -35,11 +72,15 @@ export default function App() {
       tabBarActiveTintColor: "#5F366E",
       tabBarInactiveTintColor: "gray",
     })}>
-      <Tab.Screen name="Tienda" component={Tienda} />
+      <Tab.Screen name="Tienda" component={Tienda} options={{
+        headerRight: () => (
+          <TouchableOpacity style={{backgroundColor: "red"}} onPress={handleSignOut}>
+              <Ionicons name={'ios-log-out'} size= {20} color= {'white'} />
+          </TouchableOpacity>
+        )
+      }} />
       <Tab.Screen name="Perfil" component={Perfil}  />
     </Tab.Navigator>
-    </NavigationContainer>
   );
 }
-
 
--- pages/Registro/Registro.js
+++ pages/Registro/Registro.js
@@ -1,8 +1,93 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { auth, db } from "../../firebase";
+
+
+import { StyledButtonNoneColor, StyledButtons } from '../../StyledComponents/Buttons/buttons';
+import { StyledInput, StyledInputInLine, StyledInputInLineLeft, StyledInputInLineRight } from '../../StyledComponents/Inputs/inputs';
+import { StyledTextButton, StyledTextButtonColor } from '../../StyledComponents/Text/text';
+import { StyledView, StyledViewInLineInputs } from '../../StyledComponents/Views/view';
+
+
+
 
 export default function Registro() {
+  const navigation = useNavigation();
+  const[name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [addres, setAddres] = useState("");
+  const [country, setCountry] = useState("");
+  const [phone, setPhone] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  
+  
+  useEffect(()=> {
+    const unsuscribe = auth.onAuthStateChanged((user)=>{
+      if(user){
+        navigation.replace("Tabs");
+      }
+    });
+    return unsuscribe;
+  },[]);
+
+  const handleSinUp = () =>{
+    auth.createUserWithEmailAndPassword(email ,pwd).then((userCredentials) => {
+      const user = userCredentials.user;
+      console.log(user);
+      db.ref('user').child(user.uid).set({
+        name: name,
+        email: user.email,
+        addres: addres,
+        country: country,
+        phone: phone
+      });
+    })
+    .catch((error) =>{
+      alert(error.message);
+    })
+  }
   return(
-    <Text>Registro</Text>
+    <StyledView>
+      <StyledInput
+        placeholder = "Nombre"
+        value = {name}
+        onChangeText = {(text) => setName(text)}
+      />
+      <StyledInput
+        placeholder = "Correo"
+        value = {email}
+        onChangeText = {(text) => setEmail(text) }
+      />
+      <StyledInput
+        placeholder = "Direccion"
+        value = {addres}
+        onChangeText = {(text) => setAddres(text)}
+      />
+      <StyledInput
+        placeholder = "Pais"
+        value = {country}
+        onChangeText = {(text) => setCountry(text)}
+      />
+      <StyledInput
+        placeholder = "Numero de Telefono"
+        value = {phone}
+        onChangeText = {(text) => setPhone(text)}
+      />
+      <StyledInput
+        placeholder = "Contraseña"
+        value = {pwd}
+        onChangeText = {(text) => setPwd(text)}
+        secureTextEntry
+      />
+
+      <StyledButtons onPress ={ handleSinUp}>
+        <StyledTextButton>Crear cuenta</StyledTextButton>
+      </StyledButtons>
+      <StyledButtonNoneColor onPress={() => navigation.replace('Login')}>
+        <StyledTextButtonColor>Back to Login</StyledTextButtonColor>
+      </StyledButtonNoneColor>
+    </StyledView>
   );
 }
