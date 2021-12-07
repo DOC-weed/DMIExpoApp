f5ee72248880329ca2b1ddb2eace12ba14e02bc7
erikdanielvoyastic <erikdaniel@voyastic.com>
2021-12-07 15:36:40 -0600

    cambios

--- pages/Tienda/Tienda.js
+++ pages/Tienda/Tienda.js
@@ -1,49 +1,48 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text } from 'react-native';
 import { Image, SpeedDial } from 'react-native-elements';
 import { auth, db } from '../../firebase';
+import { useIsFocused } from "@react-navigation/native";
 
 
-import { StyledImageBanner,StyledImageProduct } from '../../StyledComponents/Images/images';
+import { StyledImageBanner, StyledImageProduct } from '../../StyledComponents/Images/images';
 import { StyledTextStore } from '../../StyledComponents/Text/text';
-import { StyledViewBanner, StyledViewStore,StyledViewStoreProducts,StyledViewStoreProductsCard } from '../../StyledComponents/Views/view';
+import { StyledViewBanner, StyledViewStore, StyledViewStoreProducts, StyledViewStoreProductsCard } from '../../StyledComponents/Views/view';
 
 export default function Tienda({ navigation }) {
   const [open, setOpen] = useState(false);
   const [openAdd, setOpenAdd] = useState(false);
-  const [productsList, setproduct] =useState([]);
-  const [productsList2, setproduct2] =useState([]);
+  const [productsList, setproduct] = useState({});
+  const [productsList2, setproduct2] = useState([]);
+  const isFocused = useIsFocused();
   useEffect(() => {
     getProducts();
     getUser();
-    maps();
-
   }, [])
-  const getProducts= async()=>{
-    await db.ref().child('products').get().then(function(res){
-      console.log('child_added');
-      let json = res.val();
-      let arr =[];
-      console.log('arrJson',Object.keys(json));
-      Object.values(json).map(function(data,IdIndex){
-        arr[IdIndex]=data
-      Object.values(data).map(function(datos,IndexInterno){
-        console.log('map 2/key',datos)
 
-      })
+  useEffect(() => {
+    getProducts();
+}, [isFocused])
+  
+  const getProducts = async () => {
+    await db.ref('productsgeneral/').get().then(function (res) {
+      if (res.exists()) {
+        let json = res.val();
+        setproduct(json)
+      } else {
+        setproduct({})
+      }
 
-    })
-    setproduct(arr)
       //console.log('esto',json);
       //setproduct(json);
 
-      
-    }).catch((err)=>{
+
+    }).catch((err) => {
       console.log(err);
     })
-    maps();
+
   }
-  const getUser=()=>{
+  const getUser = () => {
     auth.onAuthStateChanged((user) => {
       if (user) {
         let uid = user.uid;
@@ -52,58 +51,43 @@ export default function Tienda({ navigation }) {
       }
     });
   }
-  const maps=()=>{
-    let container=[];
-    productsList.map((data,index)=>{
-      console.log(data,index)
-      if(Object.keys(data).length>1){
-        Object.values(data).map((datos,index2)=>{
-          console.log(datos,index2);
-          container.push(datos)
-        })
-      }
-      container.push(data)
-      console.log(Object.keys(data).length);
-    })
-    console.log(container);
-    setproduct2(container)
-  }
-  
+
+
 
   return (
     <StyledViewStore>
-      <StyledImageBanner source={require ('../../assets/banner-offer.png')}/>
+      <StyledImageBanner source={require('../../assets/banner-offer.png')} />
       <StyledViewStoreProducts
-       automaticallyAdjustContentInsets={false} // All of those are props
-       contentInSet={{ bottom: 4 }}
-       horizontal={false}
-       alwaysBounceHorizontal={false}
-       bounces={true}
-       decelerationRate="normal"
-       showsHorizontalScrollIndicator={false}
-       scrollEventThrottle={200}
-       pagingEnabled={true}
->
-        {productsList2.map((data,index)=>
-        <StyledViewStoreProductsCard key={index}>
-        <Image style={{width:'100%'}} source={data.image}/>
+        automaticallyAdjustContentInsets={false} // All of those are props
+        contentInSet={{ bottom: 4 }}
+        horizontal={false}
+        alwaysBounceHorizontal={false}
+        bounces={true}
+        decelerationRate="normal"
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={200}
+        pagingEnabled={true}
+      >
+        {Object.values(productsList).map((data, index) =>
+          <StyledViewStoreProductsCard key={index}>
+            <Image style={{ width: '100%' }} source={data.image} />
 
-          <StyledImageProduct source={{uri:data.image}}/>
-          <StyledTextStore onPress={() => navigation.navigate('Detalles',{
-            name:data.name,
-            code: data.code,
-            description:data.description,
-            image: data.image,
-            price: data.price,
-            stock: data.stock
-          })}>{data.name}</StyledTextStore>
-        </StyledViewStoreProductsCard>
-        
-        ) }
+            <StyledImageProduct source={{ uri: data.image }} />
+            <StyledTextStore onPress={() => navigation.navigate('Detalles', {
+              name: data.name,
+              code: data.code,
+              description: data.description,
+              image: data.image,
+              price: data.price,
+              stock: data.stock
+            })}>{data.name}</StyledTextStore>
+          </StyledViewStoreProductsCard>
+
+        )}
 
       </StyledViewStoreProducts>
     </StyledViewStore>
-    
+
 
   );
 }
